214e21a6eef1db0fec27b8dbe8858d9e98b74812
Paul Niemczyk <mrpndev@gmail.com>
2022-10-17 09:48:44 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("DOM Lesson")
+
+/* 
+    Data representation of the objects that comprise the structure and content of a document on the web.
+
+    
+*/
+
+console.log(typeof document)
+
